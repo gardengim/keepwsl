0be4a1fc9794a3ab375d61e1eec4d71ee1e2d2d0
Garden Gim <50333551+gardengim@users.noreply.github.com>
2025-06-14 03:20:01 +0900

    Update further steps

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 curl -o /etc/systemd/system/keepwsl.service https://raw.githubusercontent.com/gardengim/keepwsl/main/keepwsl.service 
+echo To enable the service, run:
+echo sudo systemctl enable keepwsl.service
+echo To start the service, run:
+echo sudo systemctl start keepwsl.service
